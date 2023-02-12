64bdb09a909e970ca4d9edc5200e125cc7eea6fd
volga <121410643+volgaHwang@users.noreply.github.com>
2023-02-12 22:31:20 +0900

    Time: 390 ms (93.33%), Space: 16 MB (73.33%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {        
+        var answer = 0
+        let p = people.sorted { $0 < $1 }
+        var p1 = 0, p2 = people.count - 1
+
+        while p1 < p2 {
+            if p[p1] + p[p2] <= limit {
+                answer += 1
+                p1 += 1
+                p2 -= 1
+            }else {
+                answer += 1
+                p2 -= 1
+            }
+        }
+        if p1 == p2 {
+            answer += 1
+        }
+        return answer
+    }
+}
