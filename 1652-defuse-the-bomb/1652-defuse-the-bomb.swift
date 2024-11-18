f66a9a9341f9d53f9400bcb1ff6af5dd086a55f7
volga <121410643+volgaHwang@users.noreply.github.com>
2024-11-18 12:40:44 +0900

    Time: 0 ms (100.00%), Space: 17.5 MB (56.00%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.swift
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.swift
@@ -7,36 +7,34 @@ class Solution {
             return answer
         }
         
-        // 확장된 배열을 만들어서 원형을 처리하기 쉽게 함
-        let extendedCode = code + code
         var windowSum = 0
         let absK = abs(k)
         
         if k > 0 {
             // 초기 윈도우 합 계산
             for i in 1...k {
-                windowSum += extendedCode[i]
+                windowSum += code[i % n]
             }
             answer[0] = windowSum
             
             // 슬라이딩 윈도우 이동
             for i in 1..<n {
-                windowSum += extendedCode[i + k]
-                windowSum -= extendedCode[i]
+                windowSum += code[(i + k) % n]
+                windowSum -= code[i]
                 answer[i] = windowSum
             }
         } else {
             // k < 0인 경우, 이전 abs(k)개의 합
             // 초기 윈도우 합 계산
             for i in 0..<absK {
-                windowSum += extendedCode[n - absK + i]
+                windowSum += code[(n - absK + i) % n]
             }
             answer[0] = windowSum
             
             // 슬라이딩 윈도우 이동
             for i in 1..<n {
-                windowSum += extendedCode[i - 1]
-                windowSum -= extendedCode[n - absK + i - 1]
+                windowSum += code[(i - 1) % n]
+                windowSum -= code[(n - absK + i - 1) % n]
                 answer[i] = windowSum
             }
         }
