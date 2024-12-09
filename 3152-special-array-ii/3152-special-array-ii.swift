fa7506441a8ddc8700d0648846e5f5985557f82d
volga <121410643+volgaHwang@users.noreply.github.com>
2024-12-10 08:02:59 +0900

    Time: 3 ms (100.00%), Space: 27.5 MB (50.00%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.swift
+++ 3152-special-array-ii/3152-special-array-ii.swift
@@ -1,36 +1,33 @@
 class Solution {
     func isArraySpecial(_ nums: [Int], _ queries: [[Int]]) -> [Bool] {
-        var answer: [Bool] = []
-        var ll: [Bool] = []
-        var rr: [Int] = []
+        let n = nums.count
+        // bad[i]: nums[i]와 nums[i+1]의 홀짝성이 같다면 1, 아니면 0
+        // bad 배열의 크기는 n-1
+        var bad = [Int](repeating: 0, count: n-1)
         
-        for i in 0..<nums.count {
-            if i == 0 {
-                ll.append(true)
-                continue
-            }
-            ll.append(isEven(nums[i]) != isEven(nums[i - 1]))
+        for i in 0..<n-1 {
+            // 홀짝 비교: (nums[i] & 1) == (nums[i+1] & 1)
+            bad[i] = ((nums[i] & 1) == (nums[i+1] & 1)) ? 1 : 0
         }
         
-        var m = 0
-        for i in 0..<ll.count {
-            if ll[i] == false {
-                m = i
-            }
-            rr.append(m)
+        // prefix sum 준비
+        // prefix[i]: bad[0] ~ bad[i-1]까지의 합
+        var prefix = [Int](repeating: 0, count: n)
+        for i in 1..<n {
+            prefix[i] = prefix[i-1] + bad[i-1]
         }
+
+        var result = [Bool]()
+        result.reserveCapacity(queries.count)
         
         for q in queries {
-            let i = q[0]
-            let j = q[1]
-            
-            let left = rr[i], right = rr[j]
-            answer.append(left == right)
+            let l = q[0], r = q[1]
+            // [l..r]구간에서 bad pair 개수 = prefix[r] - prefix[l]
+            // bad pair가 0이면 special
+            let count = prefix[r] - prefix[l]
+            result.append(count == 0)
         }
-        return answer
-    }
-    
-    func isEven(_ v: Int) -> Bool {
-        return (v % 2 == 0)
+        
+        return result
     }
 }
